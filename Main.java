7a59dcb971630f35a731ba5c052e29512e464717
Ryzaxd <nick986h@edu.zealand.dk>
2023-10-12 13:43:29 +0200

    first commit

---
+++ Main.java
@@ -0,0 +1,50 @@
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.SecureRandom;
+import java.util.Base64;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string to encrypt: ");
+        String input = scanner.nextLine();
+        try {
+            byte[] key = generateKey(256);
+            String encrypted = AES.encrypt(input, key);
+            System.out.println("Encrypted string: " + encrypted);
+            String decrypted = AES.decrypt(encrypted, key);
+            System.out.println("Decrypted string: " + decrypted);
+            scanner.close();
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    public static class AES {
+        private static final String ALGORITHM = "AES";
+
+        public static String encrypt(String value, byte[] key) throws Exception {
+            SecretKeySpec keySpec = new SecretKeySpec(key, ALGORITHM);
+            Cipher cipher = Cipher.getInstance(ALGORITHM);
+            cipher.init(Cipher.ENCRYPT_MODE, keySpec);
+            byte[] encryptedValue = cipher.doFinal(value.getBytes());
+            return Base64.getEncoder().encodeToString(encryptedValue);
+        }
+
+        public static String decrypt(String value, byte[] key) throws Exception {
+            SecretKeySpec keySpec = new SecretKeySpec(key, ALGORITHM);
+            Cipher cipher = Cipher.getInstance(ALGORITHM);
+            cipher.init(Cipher.DECRYPT_MODE, keySpec);
+            byte[] decryptedValue = cipher.doFinal(Base64.getDecoder().decode(value));
+            return new String(decryptedValue);
+        }
+    }
+
+    public static byte[] generateKey(int keyLength) throws Exception {
+        SecureRandom random = new SecureRandom();
+        byte[] key = new byte[keyLength / 8];
+        random.nextBytes(key);
+        return key;
+    }
+}
